03c7a22f03cb0a41125374e53810de2e8b712e45
Harry T. Vennik <htvennik@gmail.com>
2017-04-15 13:42:55 +0200

    facebook-login example: selective imports and dependencies.

--- examples/facebook-login/source/app.d
+++ examples/facebook-login/source/app.d
@@ -1,55 +1,76 @@
-import vibe.d;
-
-import oauth.provider.facebook;
 import oauth.webapp;
 
 shared static this()
 {
-    auto oauthSettings = new immutable(FacebookAuthSettings)(
-        "facebook.json".readFileUTF8.parseJsonString());
+    auto oauthSettings = () {
+        import vibe.core.file : readFileUTF8;
+        import vibe.data.json : parseJsonString;
+        import oauth.provider.facebook : FacebookAuthSettings;
+
+        return new immutable(FacebookAuthSettings)(
+            "facebook.json".readFileUTF8.parseJsonString());
+    } ();
 
     auto webapp = new OAuthWebapp;
     string[] scopes = [ "email" ];
 
-    auto router = new URLRouter;
+    auto router = () {
+        import vibe.http.router : URLRouter;
+        import vibe.http.server : render;
 
-    router.get("/logout", delegate void(scope req, scope res) {
-        res.terminateSession();
-        res.render!("logout.dt", req);
-    });
+        auto router = new URLRouter;
 
-    router.get("/login/facebook", delegate void(scope req, scope res) {
-        webapp.login(req, res, oauthSettings, scopes);
-        res.redirect("/");
-    });
+        router.get("/logout", delegate void(scope req, scope res) {
+            res.terminateSession();
+            res.render!("logout.dt", req);
+        });
 
-    router.any("*", delegate void(scope req, scope res) {
-        if (!webapp.isLoggedIn(req, oauthSettings))
-            webapp.login(req, res, oauthSettings, null, scopes);
-    });
+        router.get("/login/facebook", delegate void(scope req, scope res) {
+            webapp.login(req, res, oauthSettings, scopes);
+            res.redirect("/");
+        });
 
-    router.get("/", delegate (req, res) {
-        assert (webapp.isLoggedIn(req, oauthSettings));
+        router.any("*", delegate void(scope req, scope res) {
+            if (!webapp.isLoggedIn(req, oauthSettings))
+                webapp.login(req, res, oauthSettings, null, scopes);
+        });
 
-        auto session = webapp.oauthSession(req);
-        Json userInfo;
+        router.get("/", delegate (req, res) {
+            import vibe.http.client : requestHTTP;
+            import vibe.data.json : Json;
 
-        requestHTTP(
-            "https://graph.facebook.com/me?fields=email,first_name,last_name",
-            delegate (scope graphReq) { session.authorizeRequest(graphReq); },
-            delegate (scope graphRes) {
-                auto obj = graphRes.readJson();
-                if ("error" !in obj)
-                    userInfo = obj;
-            });
+            assert (webapp.isLoggedIn(req, oauthSettings));
 
-        res.render!("index.dt", userInfo);
-    });
+            auto session = webapp.oauthSession(req);
+            Json userInfo;
 
-    auto settings = new HTTPServerSettings;
-    settings.sessionStore = new MemorySessionStore;
-    settings.port = 8080;
+            requestHTTP(
+                "https://graph.facebook.com/me?fields=email,first_name,last_name",
+                delegate (scope graphReq) {
+                    session.authorizeRequest(graphReq);
+                },
+                delegate (scope graphRes) {
+                    auto obj = graphRes.readJson();
+                    if ("error" !in obj)
+                        userInfo = obj;
+                }
+            );
 
-    listenHTTP(settings, router);
+            res.render!("index.dt", userInfo);
+        });
+
+        return router;
+    } ();
+
+    {
+        import vibe.http.server : HTTPServerSettings, listenHTTP;
+        import vibe.http.session : MemorySessionStore;
+
+        auto settings = new HTTPServerSettings;
+        settings.sessionStore = new MemorySessionStore;
+        settings.port = 8080;
+
+        listenHTTP(settings, router);
+    }
 }
 
